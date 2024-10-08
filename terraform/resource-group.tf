76c21959ed66328987b19bec78a39cfb839bb0c5
enformatique11 <87582840+enformatique11@users.noreply.github.com>
2024-10-08 13:56:44 +0100

    Update resource-group.tf

--- terraform/resource-group.tf
+++ terraform/resource-group.tf
@@ -1,5 +1,5 @@
 module "resourcegroups" {
   source   = "./modules/resourcegroups"
-  name     = var.resource_group_name
+  resource_group_name = var.resource_group_name
   location = var.location
 }
