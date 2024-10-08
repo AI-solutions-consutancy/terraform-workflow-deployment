41dff32bce99bb4d0e9235ccf0437e44dec3de51
enformatique11 <87582840+enformatique11@users.noreply.github.com>
2024-10-08 11:32:13 +0100

    Update main.tf

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -1,6 +1,6 @@
 
 # Create a resource group
-resource "azurerm_resource_group" "resource_group" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
