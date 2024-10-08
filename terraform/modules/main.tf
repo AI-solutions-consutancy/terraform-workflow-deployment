470ccec1404b1ec456bcea225ced6c8e9bbbdb97
enformatique11 <87582840+enformatique11@users.noreply.github.com>
2024-10-08 10:54:05 +0100

    Update main.tf

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -1,8 +1,10 @@
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "= v4.4.0"
     }
+
 # Create a resource group
 resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
