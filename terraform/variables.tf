72c5304fe390d0376cc00471c07359c1eafa615e
enformatique11 <87582840+enformatique11@users.noreply.github.com>
2024-10-08 09:59:31 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,9 @@
+variable "resource_group_name" {
+    type = string
+    description = "Name of RG"
+  }
 
+variable "location" {
+    type = string
+    description = "The location of Resource deployed"
+  }
