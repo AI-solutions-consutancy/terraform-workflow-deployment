25551cb7cb097adcf5b537780832c8cc005321d3
enformatique11 <87582840+enformatique11@users.noreply.github.com>
2024-10-08 22:35:10 +0100

    Update storage-account.tf

--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -2,7 +2,7 @@ module "storageaccount" {
   source                           = "./modules/storageaccount"
   name                             = "testfrdf54"
   resource_group_name              = var.resource_group_name
-  location                         = var.resource_group_name
+  location                         = var.location
   storage_account_tier             = var.storage_account_tier
   storage_account_replication_type = var.storage_account_replication_type
 }
