802dc6f120be464e4c73750b7f298dd502900daa
enformatique11 <87582840+enformatique11@users.noreply.github.com>
2024-10-08 22:41:18 +0100

    Update main.tf

--- terraform/modules/storageaccount/main.tf
+++ terraform/modules/storageaccount/main.tf
@@ -4,5 +4,10 @@ resource "azurerm_storage_account" "stg" {
   location                 = var.location
   account_tier             = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
+}
 
+resource "azurerm_storage_container" "stg_container" {
+  name                  = "var.container_name"
+  storage_account_name  = azurerm_storage_account.stg.name
+  container_access_type = "private"
 }
