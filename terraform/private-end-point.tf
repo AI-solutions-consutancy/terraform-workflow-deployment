59f9542d8408bcf2907bab5cae48d63d0b763eda
enformatique11 <87582840+enformatique11@users.noreply.github.com>
2024-10-10 00:28:48 +0100

    Update private-end-point.tf

--- terraform/private-end-point.tf
+++ terraform/private-end-point.tf
@@ -6,5 +6,5 @@ resource_group_name             = var.resource_group_name
 location                        = var.location
 subnet_id                       = data.azurerm_subnet.pe_subnet.id
 privateserviceconnection_name   = var.privateserviceconnection_name
-connection_id                   = "/subscriptions/1433f151-b71e-4bff-b991-2f7dba179f6a/resourceGroups/ai-uks-dev-rg001/providers/Microsoft.KeyVault/vaults/keyvaultsre23"
+connection_id                   = ""
 }
