48ff2009e2b9c26a2bddeb53c6617fa1f18b1267
enformatique11 <87582840+enformatique11@users.noreply.github.com>
2024-10-10 00:35:57 +0100

    Update private-end-point.tf

--- terraform/private-end-point.tf
+++ terraform/private-end-point.tf
@@ -1,3 +1,4 @@
+/*
 module "privateendpoint" {
     source = "./modules/privateendpoint"
   
@@ -8,3 +9,4 @@ subnet_id                       = data.azurerm_subnet.pe_subnet.id
 privateserviceconnection_name   = var.privateserviceconnection_name
 connection_id                   = "/subscriptions/1433f151-b71e-4bff-b991-2f7dba179f6a/resourceGroups/ai-uks-dev-rg001/providers/Microsoft.Network/virtualNetworks/vnet1/subnets/general-subnet"
 }
+*/
