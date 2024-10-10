28bd67bb58a7c3696d2b7c3e08c8d80dbf3ed868
enformatique11 <enformatique@hotmail.com>
2024-10-10 18:42:15 +0100

    update

--- terraform/modules/storageaccount/main.tf
+++ terraform/modules/storageaccount/main.tf
@@ -1,29 +1,14 @@
-
 resource "azurerm_storage_account" "stg" {
-  account_replication_type          = var.account_replication_type
-  account_tier                      = var.account_tier
-  location                          = var.location
-  name                              = var.name
-  resource_group_name               = var.resource_group_name
-  access_tier                       = var.account_kind == "BlockBlobStorage" && var.account_tier == "Premium" ? null : var.access_tier
-  account_kind                      = var.account_kind
-  allow_nested_items_to_be_public   = var.allow_nested_items_to_be_public
-  allowed_copy_scope                = var.allowed_copy_scope
-  cross_tenant_replication_enabled  = var.cross_tenant_replication_enabled
-  default_to_oauth_authentication   = var.default_to_oauth_authentication
-  edge_zone                         = var.edge_zone
-  # https_traffic_only_enabled        = var.https_traffic_only_enabled
-  infrastructure_encryption_enabled = var.infrastructure_encryption_enabled
-  is_hns_enabled                    = var.is_hns_enabled
-  large_file_share_enabled          = var.large_file_share_enabled
-  min_tls_version                   = var.min_tls_version
-  nfsv3_enabled                     = var.nfsv3_enabled
-  public_network_access_enabled     = var.public_network_access_enabled
-  queue_encryption_key_type         = var.queue_encryption_key_type
-  sftp_enabled                      = var.sftp_enabled
-  shared_access_key_enabled         = var.shared_access_key_enabled
-  table_encryption_key_type         = var.table_encryption_key_type
+  name                     = var.name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication_type
+ # public_network_access_enabled = false
+  
+  identity {
+    type         = "SystemAssigned"
+  }
 
   
-  tags                              = var.tags
 }
--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -1,30 +1,17 @@
 module "storageaccount" {
   source                           = "./modules/storageaccount"
-  account_replication_type      = "GRS"
-  account_tier                  = "Standard"
-  account_kind                  = "StorageV2"
-  location                      = var.location
-  name                          = "test76865"
-  https_traffic_only_enabled    = true
-  resource_group_name           = var.resource_group_name
-  min_tls_version               = "TLS1_2"
-  shared_access_key_enabled     = true
-  public_network_access_enabled = true
-  large_file_share_enabled      = true
-  
-  tags = {
-    env   = "Dev"
-    owner = "Shahid"
-    dept  = "IT"
-  }
+  name                             = "testfrdf54"
+  resource_group_name              = var.resource_group_name
+  location                         = var.location
+  storage_account_tier             = var.storage_account_tier
+  storage_account_replication_type = var.storage_account_replication_type
+    
 }
   
-# resource "azurerm_storage_container" "stg_container" {
-#   depends_on = [ module.storageaccount ]
-#   name                  = var.container_name
-#   storage_account_name  = module.storageaccount.name
-#   container_access_type = "private"
+resource "azurerm_storage_container" "stg_container" {
+  depends_on = [ module.storageaccount ]
+  name                  = var.container_name
+  storage_account_name  = module.storageaccount.name
+  container_access_type = "private"
    
-# }  
-
-
+}  
